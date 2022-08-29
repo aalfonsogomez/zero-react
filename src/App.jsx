b1d0a8bd5a59c0275990da913be256f64ea2ea3c
Angel <a.alfonso.gomez@gmail.com>
2022-08-29 13:22:42 +0200

    Pintamos el nombre de la mision y el año

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import { useState, useEffect} from 'react';
 import * as API from './services/launches.jsx';
 
 export function App() {
-    return <div>Hola Mundo!!</div>
+    const [launches, setLaunches ] = useState([]);
+    useEffect( () => {
+        API.getAllLaunches().then(setLaunches);
+    }, []);
+
+    return (
+        <>
+            <h1>SpaceX Launches</h1>
+            <ul>
+                { launches.map(launch => (
+                    <li key={launch.flight_number }>
+                        {launch.mission_name} ({launch.launch_year})
+                    </li>
+                ))}
+            </ul>
+        </>
+        
+    )
 }
 
