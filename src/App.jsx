0c7a646040df626033e5052bba671d999fcc90a7
Angel <a.alfonso.gomez@gmail.com>
2022-08-31 13:19:18 +0200

    Creamos componente LaunchItem y seguimos estilando

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState, useEffect} from 'react';
-import { Heading, Box, Image, Flex, Spacer, Text, Tag } from '@chakra-ui/react';
-import dayjs from "dayjs";
-import "dayjs/locale/es";
+import { Heading, Image } from '@chakra-ui/react';
 import * as API from './services/launches.jsx';
 import logo from './assets/spaceX-logo.png';
-import{ HiCalendar } from 'react-icons/hi';
+import { LaunchItem } from './components/LaunchItem.jsx';
 
 export function App() {
     const [launches, setLaunches ] = useState([]);
@@ -20,30 +18,7 @@ export function App() {
             </Heading>
             <section>
                 { launches.map(launch => (
-                    <Box key={launch.flight_number }
-                        bg="gray.200"
-                        p={4}
-                        m={4}
-                        borderRadius="lg">
-                            <Flex display="flex">
-                                <Text fontSize="2x1">
-                                    Mission <strong>{launch.mission_name}</strong> ({launch.launch_year})
-                                </Text>
-                                <Spacer />
-                                <Tag p={4} colorScheme={launch.launch_success ? "green" : "red"}>
-                                    {launch.launch_success ? "Success" : "Failure"}
-                                </Tag>
-                            </Flex>
-                            <Flex>
-                                <HiCalendar />
-                                <Text fontSize="sm" ml={1}>
-                                    {dayjs(launch.launch_date_local)
-                                        .locale("es")
-                                        .format("D MMMM, YYYY")
-                                    }
-                                </Text>
-                            </Flex>
-                    </Box>
+                    <LaunchItem key={launch.flight_number} {...launch}/>
                 ))}
             </section>
         </>
---
+++ src/components/LaunchItem.jsx
@@ -0,0 +1,37 @@
+import dayjs from "dayjs";
+import "dayjs/locale/es";
+import{ HiCalendar } from 'react-icons/hi';
+import { Box, Flex, Spacer, Text, Tag, Button, Icon } from '@chakra-ui/react';
+
+export function LaunchItem(launch) {
+    return (
+        <Box 
+            bg="gray.200"
+            p={4}
+            m={4}
+            borderRadius="lg">
+            <Flex display="flex">
+                <Text fontSize="2x1">
+                    Mission <strong>{launch.mission_name}</strong> ({launch.launch_year})
+                </Text>
+                <Spacer />
+                <Tag p={2} colorScheme={launch.launch_success ? "green" : "red"}>
+                    {launch.launch_success ? "Success" : "Failure"}
+                </Tag>
+            </Flex>
+            <Flex>
+                <Icon as={HiCalendar} color="gray.500" />
+                <Text fontSize="sm" ml={1} color="gray.500">
+                    {dayjs(launch.launch_date_local)
+                        .locale("es")
+                        .format("D MMMM, YYYY")
+                    }
+                </Text>
+            </Flex>
+            <Button mt={2} colorScheme="blue">
+                More Details
+            </Button>
+        </Box>
+
+    );
+}
