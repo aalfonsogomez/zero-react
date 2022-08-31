26fc294fec1ecbfda047481d4f3ed41953ce1801
Angel <a.alfonso.gomez@gmail.com>
2022-08-31 12:52:06 +0200

    Añadimos chaka y iconos y estilamos la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect} from 'react';
+import { Heading, Box, Image, Flex, Spacer, Text, Tag } from '@chakra-ui/react';
 import * as API from './services/launches.jsx';
 import logo from './assets/spaceX-logo.png';
+import{ HiCalendar } from 'react-icons/hi';
 
 export function App() {
     const [launches, setLaunches ] = useState([]);
@@ -10,15 +12,35 @@ export function App() {
 
     return (
         <>
-            <img src={logo} width={300} />
-            <h1>SpaceX Launches</h1>
-            <ul>
+            <Image m={4} src={logo} width={300} />
+            <Heading as="h1" size="lg" m={4}>
+                SpaceX Launches
+            </Heading>
+            <section>
                 { launches.map(launch => (
-                    <li key={launch.flight_number }>
-                        {launch.mission_name} ({launch.launch_year})
-                    </li>
+                    <Box key={launch.flight_number }
+                        bg="gray.200"
+                        p={4}
+                        m={4}
+                        borderRadius="lg">
+                            <Flex display="flex">
+                                <Text fontSize="2x1">
+                                    Mission <strong>{launch.mission_name}</strong> ({launch.launch_year})
+                                </Text>
+                                <Spacer />
+                                <Tag p={4} colorScheme={launch.launch_success ? "green" : "red"}>
+                                    {launch.launch_success ? "Success" : "Failure"}
+                                </Tag>
+                            </Flex>
+                            <Flex>
+                                <HiCalendar />
+                                <Text fontSize="sm" ml={1}>
+                                    {launch.launch_date_local.split("T")[0]}
+                                </Text>
+                            </Flex>
+                    </Box>
                 ))}
-            </ul>
+            </section>
         </>
     )
 }
